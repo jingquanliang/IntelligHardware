3b2903209c03c6458bc522c6c3e6b52c143d8285
jingquanliang <540692237@qq.com>
2018-05-04 15:16:31 +0800

    restTemplate 问题不支持 text/plain 的问题解决，可以微信登录，然后rest后台请求微信服务器，返回openid等信息的需求！

--- framework-core/src/main/java/controller/LoginController.java
+++ framework-core/src/main/java/controller/LoginController.java
@@ -5,8 +5,10 @@ import dao.bean.WxUserInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpSession;
@@ -16,56 +18,65 @@ import javax.servlet.http.HttpSession;
 @EnableAutoConfiguration
 public class LoginController {
 
-   /**
+    /**
      * 引入日志，注意都是"org.slf4j"包下
      */
     private final static Logger logger = LoggerFactory.getLogger(SampleController.class);
 
-    @RequestMapping("/login")
-    String loginFromWX(HttpSession session,@RequestParam(value="type")int type,@RequestParam(value="code")String code) {
+    @Autowired
+    private RestTemplate restTemplate;
 
-        WxUserInfo userInfo=null;
-        if(type==0){
+    @Autowired
+    private RestOperations restOperations;
+
+    @RequestMapping("/login")
+    String loginFromWX(HttpSession session, @RequestParam(value = "type") int type, @RequestParam(value = "code") String code) {
+
+        WxUserInfo userInfo = null;
+        if (type == 0) {
             //from weixin
             logger.info("from weixin");
 
-            RestTemplate restTemplate = new RestTemplate();
-            String APPID="wxe75bc768e291fd04";
-            String SECRET="9afb6fc9e310fea8e4350266ab3fa641";
-            String JSCODE=code;
-            String url="https://api.weixin.qq.com/sns/jscode2session?appid="+APPID+"&secret="+SECRET+"&js_code="+JSCODE+"&grant_type=authorization_code";
+//            RestTemplate restTemplate = new RestTemplate();
+            String APPID = "wxe75bc768e291fd04";
+            String SECRET = "9afb6fc9e310fea8e4350266ab3fa641";
+            String JSCODE = code;
+            String url = "https://api.weixin.qq.com/sns/jscode2session?appid=" + APPID + "&secret=" + SECRET + "&js_code=" + JSCODE + "&grant_type=authorization_code";
             userInfo = restTemplate.getForObject(url, WxUserInfo.class);
             logger.info(userInfo.toString());
+//            String userInfoStr = restTemplate.getForObject(url, String.class);
+//            logger.info(userInfoStr);
+
         }
-        if(userInfo.getErrcode()!=40029)
-            return Host.getIPAndHostName()+"-getOpenid="+userInfo.getOpenid()+"-getSession_key="+userInfo.getSession_key();
+        if (userInfo.getErrcode() != 40029)
+            return Host.getIPAndHostName() + "-getOpenid=" + userInfo.getOpenid() + "-getSession_key=" + userInfo.getSession_key();
         else
             return "0";
     }
 
     @RequestMapping("/loginFromApp")
-    String loginFromApp(HttpSession session,@RequestParam(value="type")int type,@RequestParam(value="code")String code,@RequestParam(value="name")String name,@RequestParam(value="password")String password) {
+    String loginFromApp(HttpSession session, @RequestParam(value = "type") int type, @RequestParam(value = "code") String code, @RequestParam(value = "name") String name, @RequestParam(value = "password") String password) {
 
-        if(type==0){
+        if (type == 0) {
             //from weixin
             logger.info("from weixin");
 
             RestTemplate restTemplate = new RestTemplate();
-            String APPID="wxe75bc768e291fd04";
-            String SECRET="9afb6fc9e310fea8e4350266ab3fa641";
-            String JSCODE=code;
-            String url="https://api.weixin.qq.com/sns/jscode2session?appid="+APPID+"&secret="+SECRET+"&js_code="+JSCODE+"&grant_type=authorization_code";
+            String APPID = "wxe75bc768e291fd04";
+            String SECRET = "9afb6fc9e310fea8e4350266ab3fa641";
+            String JSCODE = code;
+            String url = "https://api.weixin.qq.com/sns/jscode2session?appid=" + APPID + "&secret=" + SECRET + "&js_code=" + JSCODE + "&grant_type=authorization_code";
             WxUserInfo quote = restTemplate.getForObject(url, WxUserInfo.class);
             logger.info(quote.toString());
             logger.info(quote.toString());
 
-        }else {
+        } else {
             //from app or web
-            logger.info("name="+name);
-            logger.error("password="+password);
-            session.setAttribute(name,password);
+            logger.info("name=" + name);
+            logger.error("password=" + password);
+            session.setAttribute(name, password);
         }
-        return Host.getIPAndHostName()+"-name="+name+"-password="+password;
+        return Host.getIPAndHostName() + "-name=" + name + "-password=" + password;
     }
 
 
