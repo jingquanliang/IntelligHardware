1e1c8f2346f15a7ac6fbb05aeeae459f9e875116
jingquanliang <540692237@qq.com>
2018-09-01 10:16:53 +0800

    由于删除文件过大，提交不成功，没有办法，只能pull下来，复制文件进去，然后重新上传

--- framework-pcweb/docker/onekey.sh
+++ framework-pcweb/docker/onekey.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-gradle clean
-gradle build
+#gradle clean
+#gradle build
+rm -f jar-web-content-0.1.0.jar
+rm -f jdk-8u171-linux-x64.tar.gz
+
+cp ../build/libs/jar-web-content-0.1.0.jar ./ ##工Dockerfile文件使用
+
+cp ../../jdk-8u171-linux-x64.tar.gz ./  ##工Dockerfile文件使用
+
 
 # docker stop jcore
 # docker rm jcore
@@ -11,5 +18,5 @@ gradle build
 
 # docker run -d -p 9999:9999 --name jcore  jing/core
 
-docker-compose down
+docker-compose down --rmi all
 docker-compose up -d
